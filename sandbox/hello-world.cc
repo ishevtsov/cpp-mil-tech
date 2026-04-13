f8206d0eae450811ea71dd03ed42fac4ed5bad59
Igor Shevtsov <shevtsov@google.com>
2026-04-13 19:47:07 +0100

    Rename file extention .cpp to .cc

--- sandbox/hello-world.cc
+++ sandbox/hello-world.cc
@@ -4,12 +4,5 @@ int main()
 {
     std::cout << "Hello, World!" << std::endl;
 
-    int a[] = {1, 2, 3, 4, 5};
-    for (int i : a)
-    {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-
     return 0;
 }
